d345d8b9f216b9e6f4b6c96c681e5084dc840467
setiawandani <danistwn.004@gmail.com>
2024-05-06 15:43:26 +0700

    latihan6

---
+++ latihan6/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>css Card</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-img">
+                <img src="anime.jpg" alt="">
+            </div>
+            <div class="card-body">
+            <h2 class="card-title">The Boy and The heroin</h2>
+            <p class=""card-inro>
+            Kimitachi wa Dō Ikiru ka adalah sebuah film animasi fantasi Jepang tahun 2023 
+            yang ditulis dan disutradarai oleh Hayao Miyazaki dan diproduksi oleh Studio Ghibli.
+            Judulnya merujuk pada novel berjudul sama karangan Genzaburō Yoshino terbit pada tahun 1937 yang muncul dalam film,
+            namun memiliki cerita orisinal tersendiri dan tidak ada hubungannya dengan cerita pada novel. 
+            </p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
