35fda2026d958b23acb29aa64dbc0fc88a167285
setiawandani <danistwn.004@gmail.com>
2024-05-06 13:12:59 +0700

    latihan5

---
+++ latihan5/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>membuat subsribe</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <a href="#" class="btn">Subscribe</a>
+    <a href="#" class="cmn">Comment</a>
+    <a href="#" class="like">Like</a>
+
+</body>
+</html>
